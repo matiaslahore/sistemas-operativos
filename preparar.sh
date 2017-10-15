75dd2830187e17ffd25867f10bf64f8e2e37c69a
Joaquin Casal <joaquinarielcasal@gmail.com>
2017-10-15 17:53:49 -0300

    Correciones varias de integracion

--- preparar.sh
+++ preparar.sh
@@ -8,8 +8,9 @@
 # - El sistema no puede ser inicializado																	  #
 ###############################################################################################################
 
-export GRUPO=$HOME/GRUPO02
-export CONFDIR=$GRUPO/dirconf
+export GRUPO="$HOME"/Grupo02
+export CONFDIR="$GRUPO"/dirconf
+CONFARCH="$CONFDIR"/config.conf
 
 #chmod +w $LOGS/preparar.log
 chmod +x loger.sh
@@ -39,7 +40,7 @@ done
 if [ $continuar_proceso = "false" ]; then
 	echo "Ambiente ya inicializado, si quiere reiniciar termine su sesión e ingrese nuevamente"
 	sleep 3
-	./$LOGER "$SCRIPT" "Alerta" "Ambiente ya inicializado" "$LOGS/preparar.log"
+	./$LOGER "$SCRIPT" "Alerta" "Ambiente ya inicializado" "$LOGS"/preparar.log
 	exit 
 fi
 
@@ -47,60 +48,58 @@ fi
 # Setear las variables de ambiente #
 ####################################
 
-cd $CONFDIR/
-
 echo "Seteando variables de ambiente..."
 
-LOGS=$(grep "logs" "config.txt")
+LOGS=$(grep "logs" "$CONFARCH")
 LOGS=$(echo $LOGS| cut -d'-' -f2)
 export LOGS
-./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable LOGS" "$LOGS/preparar.log"
+./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable LOGS" "$LOGS"/preparar.log
 
-EJECUTABLES=$(grep "ejecutables" "config.txt")
+EJECUTABLES=$(grep "ejecutables" "$CONFARCH")
 EJECUTABLES=$(echo $EJECUTABLES| cut -d'-' -f2)
 export EJECUTABLES
-./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable EJECUTABLES" "$LOGS/preparar.log"
+./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable EJECUTABLES" "$LOGS"/preparar.log
 
-MAESTROS=$(grep "maestros" "config.txt")
+MAESTROS=$(grep "maestros" "$CONFARCH")
 MAESTROS=$(echo $MAESTROS| cut -d'-' -f2)
 export MAESTROS
-./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable MAESTROS" "$LOGS/preparar.log"
+./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable MAESTROS" "$LOGS"/preparar.log
 
-ACEPTADOS=$(grep "aceptados" "config.txt")
+ACEPTADOS=$(grep "aceptados" "$CONFARCH")
 ACEPTADOS=$(echo $ACEPTADOS| cut -d'-' -f2)
 export ACEPTADOS
-./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable ACEPTADOS" "$LOGS/preparar.log"
+./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable ACEPTADOS" "$LOGS"/preparar.log
 
-RECHAZADOS=$(grep "rechazados" "config.txt")
+RECHAZADOS=$(grep "rechazados" "$CONFARCH")
 RECHAZADOS=$(echo $RECHAZADOS| cut -d'-' -f2)
 export RECHAZADOS
-./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable RECHAZADOS" "$LOGS/preparar.log"
+./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable RECHAZADOS" "$LOGS"/preparar.log
 
-VALIDADOS=$(grep "validados" "config.txt")
+VALIDADOS=$(grep "validados" "$CONFARCH")
 VALIDADOS=$(echo $VALIDADOS| cut -d'-' -f2)
 export VALIDADOS
-./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable VALIDADOS" "$LOGS/preparar.log"
+./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable VALIDADOS" "$LOGS"/preparar.log
 
-REPORTES=$(grep "reportes" "config.txt")
+REPORTES=$(grep "reportes" "$CONFARCH")
 REPORTES=$(echo $REPORTES| cut -d'-' -f2)
 export REPORTES
-./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable REPORTES" "$LOGS/preparar.log"
+./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable REPORTES" "$LOGS"/preparar.log
 
-./$LOGER "$SCRIPT" "Informativo" "Las variables fueron seteadas exitosamente" "$LOGS/preparar.log"
+./$LOGER "$SCRIPT" "Informativo" "Las variables fueron seteadas exitosamente" "$LOGS"/preparar.log
 
 
 ##############################################
 # Verificar que la instalacion este completa #
 ##############################################
 
-for LINEA in $(cat config.txt); do
+for LINEA in $(cat "$CONFARCH"); do
 
 	path=$(echo $LINEA | cut -d'-' -f2 )
 
 	if [ -d $path ]; then
-		./$LOGER "$SCRIPT" "Informativo" "El directorio $path existe" "$LOGS/preparar.log"
+		./$LOGER "$SCRIPT" "Informativo" "El directorio $path existe" "$LOGS"/preparar.log
 	else
-		./$LOGER "$SCRIPT" "Error" "No se encontro el directorio $path, es necesario ejecutar el instalador instalador.sh" "$LOGS/preparar.log"
+		./$LOGER "$SCRIPT" "Error" "No se encontro el directorio $path, es necesario ejecutar el instalador instalador.sh" "$LOGS"/preparar.log
 		echo "No se encontro el directorio $path, es necesario ejecutar el instalador instalador.sh"
 		sleep 5
 		exit 
@@ -108,44 +107,35 @@ for LINEA in $(cat config.txt); do
 
 done
 
-./$LOGER "$SCRIPT" "Informativo" "La instalacion esta completa" "$LOGS/preparar.log"
+./$LOGER "$SCRIPT" "Informativo" "La instalacion esta completa" "$LOGS"/preparar.log
 
 ############################################################
 # Verificar que los archivos tengan los permisos adecuados #
 ############################################################
 
 count=1
-for LINEA in $(cat config.txt); do
+for LINEA in $(cat "$CONFARCH"); do
 
 	path=$(echo $LINEA | cut -d'-' -f2)
-	cd $path
 
 	if [ $count -eq 1 ]; then
-		for NOMBRE in $( ls ); do
+		for NOMBRE in $( ls "$path" ); do
 			if [ -x $NOMBRE ]; then
-				cd $CONFDIR/
-				./$LOGER "$SCRIPT" "Informativo" "El archivo $NOMBRE tiene permisos de ejecucion" "$LOGS/preparar.log"
+				./$LOGER "$SCRIPT" "Informativo" "El archivo $NOMBRE tiene permisos de ejecucion" "$LOGS"/preparar.log
 			else
-				cd $CONFDIR/
-				./$LOGER "$SCRIPT" "Alerta" "El archivo $NOMBRE no tiene permisos de ejecucion, seteando permisos..." "$LOGS/preparar.log"
-				cd $path
-				chmod +x $NOMBRE
-				cd $CONFDIR/
-				./$LOGER "$SCRIPT" "Informativo" "Permisos de ejecuccion listos" "$LOGS/preparar.log"
+				./$LOGER "$SCRIPT" "Alerta" "El archivo $NOMBRE no tiene permisos de ejecucion, seteando permisos..." "$LOGS"/preparar.log
+				chmod +x "$path"/"$NOMBRE"
+				./$LOGER "$SCRIPT" "Informativo" "Permisos de ejecuccion listos" "$LOGS"/preparar.log
 			fi
 		done
 	else
-		for NOMBRE in $( ls ); do
+		for NOMBRE in $( ls "$path" ); do
 			if [ -r $NOMBRE ]; then
-				cd $CONFDIR/
-				./$LOGER "$SCRIPT" "Informativo" "El archivo $NOMBRE tiene permisos de lectura" "$LOGS/preparar.log"
+				./$LOGER "$SCRIPT" "Informativo" "El archivo $NOMBRE tiene permisos de lectura" "$LOGS"/preparar.log
 			else
-				cd $CONFDIR/
-				./$LOGER "$SCRIPT" "Alerta" "El archivo $NOMBRE no tiene permisos de lectura, seteando permisos..." "$LOGS/preparar.log"
-				cd $path
-				chmod +r $NOMBRE
-				cd $CONFDIR/
-				./$LOGER "$SCRIPT" "Informativo" "Permisos de escritura listos" "$LOGS/preparar.log"
+				./$LOGER "$SCRIPT" "Alerta" "El archivo $NOMBRE no tiene permisos de lectura, seteando permisos..." "$LOGS"/preparar.log
+				chmod +r "$path"/"$NOMBRE"
+				./$LOGER "$SCRIPT" "Informativo" "Permisos de escritura listos" "$LOGS"/preparar.log
 			fi
 		done	
 	fi
@@ -171,7 +161,7 @@ done
 
 export DIRABUS
 
-./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable DIRABUS exitosamente" "$LOGS/preparar.log"
+./$LOGER "$SCRIPT" "Informativo" "Se exporto la variable DIRABUS exitosamente" "$LOGS"/preparar.log
 
 #############################
 # Invocar al script demonio #
@@ -188,7 +178,7 @@ IDPROCESO=$(echo $PROCESO| cut -d' ' -f 1)
 
 echo "Se ha invocado al demonio, su ID de proceso es: $IDPROCESO"
 
-./$LOGER "$SCRIPT" "Informativo" "Se ejecuto el script demonio.sh, su ID es $IDPROCESO" "$LOGS/preparar.log"
+./$LOGER "$SCRIPT" "Informativo" "Se ejecuto el script demonio.sh, su ID es $IDPROCESO" "$LOGS"/preparar.log
 
 
 echo "Para finalizar el demonio escriba: ./stop.sh"
